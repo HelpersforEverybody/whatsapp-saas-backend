54d66aee798925dc1fa13854a43d16e0ccefe0f1
HelpersforEverybody <helpersforeverybody@gmail.com>
2025-10-06 23:15:34 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import ShopManager from "./pages/ShopManager";
