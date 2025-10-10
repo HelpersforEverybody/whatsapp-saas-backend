775484fd45f4ada5faa13c211d9dcecacf121010
HelpersforEverybody <helpersforeverybody@gmail.com>
2025-10-10 19:04:06 +0530

    Create counter.js

---
+++ models/counter.js
@@ -0,0 +1,9 @@
+// backend/models/counter.js
+const mongoose = require('mongoose');
+
+const CounterSchema = new mongoose.Schema({
+  _id: String,
+  seq: { type: Number, default: 0 }
+});
+
+module.exports = mongoose.model('Counter', CounterSchema);
